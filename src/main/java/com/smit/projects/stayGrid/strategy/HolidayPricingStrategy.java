149ed3cb8e3af5cc3a2da2ec9d22d274d6637d93
Smit Roy <smitroy22@gmail.com>
2026-05-18 06:14:26 +0530

    4. Dynamic Pricing Engine and Pricing Scheduler added.

---
+++ src/main/java/com/smit/projects/stayGrid/strategy/HolidayPricingStrategy.java
@@ -0,0 +1,28 @@
+package com.smit.projects.stayGrid.strategy;
+
+import com.smit.projects.stayGrid.entity.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+public class HolidayPricingStrategy implements PricingStrategy{
+
+    private final PricingStrategy wrapped;
+
+    @Override
+    public BigDecimal calculatePrice(Inventory inventory) {
+
+        BigDecimal price = wrapped.calculatePrice(inventory);
+
+        //NOTE: call an API or check with local date
+        boolean isTodayHoliday = true;
+
+        if(isTodayHoliday){
+            price = price.multiply(BigDecimal.valueOf(1.25));
+        }
+
+        return price;
+    }
+}
---
+++ src/main/java/com/smit/projects/stayGrid/strategy/OccupancyPricingStrategy.java
@@ -0,0 +1,23 @@
+package com.smit.projects.stayGrid.strategy;
+
+import com.smit.projects.stayGrid.entity.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+public class OccupancyPricingStrategy implements PricingStrategy{
+
+    private final PricingStrategy wrapped;
+
+    @Override
+    public BigDecimal calculatePrice(Inventory inventory) {
+        BigDecimal price = wrapped.calculatePrice(inventory);
+        double occupancyRate = (double) inventory.getBookedCount() / inventory.getReservedCount();
+        if(occupancyRate > 0.8){
+            price = price.multiply(BigDecimal.valueOf(1.2));
+        }
+        return price;
+    }
+}
