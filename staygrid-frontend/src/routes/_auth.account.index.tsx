35b83a23249931b0874f932bd6936900fefd5a10
Smit Roy <smitroy22@gmail.com>
2026-07-04 01:30:55 +0530

    15. Removed frontend repo from backend

--- staygrid-frontend/src/routes/_auth.account.index.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_auth/account/")({
-  beforeLoad: () => { throw redirect({ to: "/account/profile" }); },
-});
--- staygrid-frontend/src/routes/_mgr.manager.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { DashboardLayout } from "@/components/layout/DashboardLayout";
-
-export const Route = createFileRoute("/_mgr/manager")({
-  component: () => <DashboardLayout><Outlet /></DashboardLayout>,
-});
